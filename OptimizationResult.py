513f6bbf6f180219f6445d67fbf8f6d274fef85e
Torok Mark Patrik <torok.mark.patrik@gmail.com>
2017-10-11 10:52:13 +0200

    Now multiple results with uniformly named directories can be evaluated automatically (examples provided). Plus did some debugging and refactoring.

--- OptimizationResult.py
+++ OptimizationResult.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import os
+import re
 import abc
 import time
 import numpy as np
@@ -7,9 +8,6 @@ import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-
-
-cwd = os.getcwd()
 """
 NOTE: file formats used:
 	-ind_file: 	generation, population size, (fitnes results), [resulting parameters]
@@ -26,8 +24,11 @@ class OptimizationSettings(MetaOptimizationSettings):
 	Class for getting and storing the needed settings of the optimization process.
 	'''
 
-	def __init__(self, xml_file):
-		self.xml_file = xml_file
+	def __init__(self, xml_file="_settings.xml", directory=''):
+		self.LAST_ELEMENT_INDEX = -1
+
+		self.directory = directory
+		self.xml_file = self.directory + xml_file
 
 	def getOptimizationSettings(self):
 		xml = ET.parse(self.xml_file)
@@ -37,7 +38,7 @@ class OptimizationSettings(MetaOptimizationSettings):
 			if child.tag == "evo_strat":
 				self.algorithm_name = child.text
 			if child.tag == "model_path":
-				self.model_name = child.text.split('/')[-1]
+				self.model_name = child.text.split('/')[self.LAST_ELEMENT_INDEX]
 			if child.tag == "boundaries":
 				self.boundaries =  map(lambda x:map(self._float_or_int,x.strip().split(", ")), child.text.strip()[2:len(child.text.strip())-2].split("], ["))
 			if child.tag == "max_evaluation":
@@ -51,7 +52,7 @@ class OptimizationSettings(MetaOptimizationSettings):
 			if child.tag == "weights":
 				self.weights =  map(self._float_or_int,child.text.strip().lstrip("[").rstrip("]").split(","))
 				self.number_of_objectives = len(self.weights)
-		return [self.algorithm_name, self.model_name, self.boundaries, self.number_of_generations, self.population_size, self.number_of_parameters,
+		return [self.directory, self.algorithm_name, self.model_name, self.boundaries, self.number_of_generations, self.population_size, self.number_of_parameters,
 					self.features, self.weights, self.number_of_objectives]
 
 	@staticmethod
@@ -72,10 +73,10 @@ class RawOptimizationResult():
 	'''
 	Class for getting and storing the result of the optimization procces.
 	'''
-	def __init__(self, opSettings, ind_file):
-		self.algorithm_name, self.model_name, self.boundaries, self.number_of_generations, self.population_size, \
+	def __init__(self, opSettings, ind_file="ind_file.txt"):
+		self.directory, self.algorithm_name, self.model_name, self.boundaries, self.number_of_generations, self.population_size, \
 									self.number_of_parameters, self.features, self.weights, self.number_of_objectives = opSettings.getOptimizationSettings()
-		self.ind_file = ind_file
+		self.ind_file = self.directory + ind_file
 		self.generations = []
 
 		self.parseIndividualFile()
@@ -103,9 +104,6 @@ class RawOptimizationResult():
 	def printRawIndividualResults(self):
 		self.printGivenIndividuals(self.generations)
 
-	def printFinalGeneration(self):
-		self.printGivenIndividuals([self.final_generation])
-
 	@staticmethod
 	def printGivenIndividuals(generations):
 		for  generation in generations:
@@ -119,7 +117,7 @@ class SortedMOOResult(RawOptimizationResult):
 	fitness score.
 	"""
 
-	def __init__(self, opSettings, ind_file):
+	def __init__(self, opSettings, ind_file='ind_file.txt'):
 		RawOptimizationResult.__init__(self, opSettings, ind_file)
 
 		self.INDEX_OF_WEIGHTED_SUM = 2
@@ -174,15 +172,15 @@ class SortedMOOResult(RawOptimizationResult):
 		plt.yscale('log')
 
 		plt.legend(fontsize=14, ncol=1)
-		plt.savefig('{0} on {1}'.format(self.algorithm_name, self.model_name), format='pdf')
+		plt.savefig(self.directory + '{0} on {1}'.format(self.algorithm_name, self.model_name), format='pdf')
 
 	def writeStatistics(self):
-		with open("sorted_stat_file.txt", "wb") as f:
+		with open(self.directory + "sorted_stat_file.txt", "wb") as f:
 			for index, generation in enumerate(self.statistics):
 				f.write('%d, %d, %s\n' % (index, self.population_size, ",".join(map(str, generation))))
 
 	def writeSortedIndividuals(self):
-		with open("sorted_ind_file.txt", "wb") as f:
+		with open(self.directory + "sorted_ind_file.txt", "wb") as f:
 			for generation in self.sorted_generations:
 				for individual in generation:
 					f.write("%s\n" % ", ".join(self.prepareIndividualForWriting(individual)))
@@ -199,12 +197,14 @@ class TrueMOOResult(RawOptimizationResult):
 	and plotting the Pareto Front.
 	"""
 
-	def __init__(self, opSettings, ind_file, final_archive_file):
+	def __init__(self, opSettings, ind_file="ind_file.txt", final_archive_file="final_archive.txt"):
 		RawOptimizationResult.__init__(self, opSettings, ind_file)
 
-		self.NUMBER_OF_OBJECTIVE = range(self.number_of_objectives)
+		self.OBJECTIVE_NUMBER = range(self.number_of_objectives)
+		self.LAST_ELEMENT_INDEX = -1
+		self.OFFSET = 2
 
-		self.final_archive_file = final_archive_file
+		self.final_archive_file = self.directory + final_archive_file
 		self.final_archive = []
 		self.final_generation = []
 		self.final_generation_objectives = []
@@ -226,25 +226,25 @@ class TrueMOOResult(RawOptimizationResult):
 		self.final_archive.append(archived_individual)
 
 	def separateFinalGeneration(self):
-		self.final_generation = self.generations[-1]
+		self.final_generation = self.generations[self.LAST_ELEMENT_INDEX]
 
 	def separateFinalGenerationObjectives(self):
-		self.final_generation_objectives = [individual[2:5] for individual in self.final_generation]
+		self.final_generation_objectives = [individual[self.OFFSET:self.OFFSET + self.number_of_objectives] for individual in self.final_generation]
 
 	def plotParetoFront(self, best_individuals, title):
 
 		x = []
 		y = []
 		for individual in best_individuals:
-			x.append(individual[self.NUMBER_OF_OBJECTIVE[0]])
-			y.append(individual[self.NUMBER_OF_OBJECTIVE[1]])
+			x.append(individual[self.OBJECTIVE_NUMBER[0]])
+			y.append(individual[self.OBJECTIVE_NUMBER[1]])
 		if self.number_of_objectives > 2:
 			fig = plt.figure()
 			ax = fig.add_subplot(111, projection='3d')
-			z = [row[self.NUMBER_OF_OBJECTIVE[2]] for row in best_individuals]
+			z = [row[self.OBJECTIVE_NUMBER[2]] for row in best_individuals]
 			ax.scatter(x, y, z, color='b')
 			ax.set_ylim(min(z), max(z))
-			ax.set_zlabel(self.features[self.NUMBER_OF_OBJECTIVE[2]])
+			ax.set_zlabel(self.features[self.OBJECTIVE_NUMBER[2]])
 		else:
 			fig = plt.figure()
 			ax = fig.add_subplot(111)
@@ -254,17 +254,33 @@ class TrueMOOResult(RawOptimizationResult):
 		fig.suptitle('{0} of {1} on {2}'.format(title,  self.algorithm_name, self.model_name))
 		ax.autoscale_view(True,True,True)
 
-		ax.set_xlabel(self.features[self.NUMBER_OF_OBJECTIVE[0]])
-		ax.set_ylabel(self.features[self.NUMBER_OF_OBJECTIVE[1]])
-		plt.savefig('{0} of {1} on {2}'.format(title, self.algorithm_name, self.model_name), format='pdf')
+		ax.set_xlabel(self.features[self.OBJECTIVE_NUMBER[0]])
+		ax.set_ylabel(self.features[self.OBJECTIVE_NUMBER[1]])
+		plt.savefig(self.directory + '{0} of {1} on {2}'.format(title, self.algorithm_name, self.model_name), format='pdf')
 
 if __name__ == '__main__':
+	CHILD_DIR_INDEX = 0
+	LAST_ELEMENT_INDEX = -1
 	start_time = time.time()
 
-	paes_clamp_opSettings = OptimizationSettings("paes_settings.xml")
-	paes_clamp_sorted_result = SortedMOOResult(paes_clamp_opSettings, "paes_ind_file.txt")
-	paes_clamp_multi_objective_result = TrueMOOResult(paes_clamp_opSettings, "paes_ind_file.txt", "paes_final_archive.txt")
-	plt.show()
+	cwd = os.getcwd()
+	hh_regex = re.compile('hh_pas_surrogate_.')
+	hh_directories = [x[CHILD_DIR_INDEX]+'/' for x in os.walk(cwd) if re.match(hh_regex, x[CHILD_DIR_INDEX].split('/')[LAST_ELEMENT_INDEX])]
+
+	vclamp_regex = re.compile('VClamp_surrogate_.')
+	vclamp_directories = [x[CHILD_DIR_INDEX]+'/' for x in os.walk(cwd) if re.match(vclamp_regex, x[CHILD_DIR_INDEX].split('/')[LAST_ELEMENT_INDEX])]
+
+	for directory in hh_directories:
+		nsga_hh_opSettings = OptimizationSettings(directory=directory)
+		nsga_hh_sorted_result = SortedMOOResult(nsga_hh_opSettings)
+		nsga_hh_multi_objective_result = TrueMOOResult(nsga_hh_opSettings)
+		plt.show()
+
+	for directory in vclamp_directories:
+		paes_clamp_opSettings = OptimizationSettings(directory=directory)
+		paes_clamp_sorted_result = SortedMOOResult(paes_clamp_opSettings)
+		paes_clamp_multi_objective_result = TrueMOOResult(paes_clamp_opSettings)
+		plt.show()
 
 	nsga_hh_opSettings = OptimizationSettings("hh_settings.xml")
 	nsga_hh_sorted_result = SortedMOOResult(nsga_hh_opSettings, "hh_ind_file.txt")

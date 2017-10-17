6f5f8bacd1cf49e1ef80bca5be442eae3f1c8c5d
Torok Mark Patrik <torok.mark.patrik@gmail.com>
2017-10-17 19:09:15 +0200

    Aliased some class methods in the script part of the code for more transparency

--- scripted/multi_objective_evaluator_script.py
+++ scripted/multi_objective_evaluator_script.py
@@ -16,8 +16,12 @@ def fill_statistics_for_all_runs(all_minimums_of_all_runs):
 	all_statistics_of_all_runs = []
 	for i in range(len(all_minimums_of_all_runs[0])):
 			current_column = [column[i] for column in all_minimums_of_all_runs]
-			all_statistics_of_all_runs.append(ORP.WeightedMooResult.calculate_statistics(current_column))
+			all_statistics_of_all_runs.append(calculate_statistics(current_column))
 	return all_statistics_of_all_runs
+
+
+def calculate_statistics(current_column):
+	return ORP.WeightedMooResult.calculate_statistics(current_column)
 
 
 def write_separate_statistics(all_statistics_of_all_runs, cwd):
@@ -28,6 +32,10 @@ def write_separate_statistics(all_statistics_of_all_runs, cwd):
 		with open("{0}{1}.txt".format(results_directory, stat_type), "wb") as f:
 			for stats in all_statistics_of_all_runs:
 				f.write('%s\n' % stats[index])
+
+
+def write_statistics(directory, statistics, population_size):
+	return ORP.WeightedMooResult.write_statistics(directory, statistics, population_size)
 
 
 def create_directory_for_statistics(cwd):
@@ -67,7 +75,7 @@ if __name__ == '__main__':
 
 	all_statistics_of_all_runs = fill_statistics_for_all_runs(all_minimums_of_all_runs)
 	write_separate_statistics(all_statistics_of_all_runs, cwd)
-	ORP.WeightedMooResult.write_statistics((cwd+'/'), all_statistics_of_all_runs, population_size)
+	write_statistics((cwd+'/'), all_statistics_of_all_runs, population_size)
 
 	plotter = POR.GeneralPlotter(algorithm_name, model_name, directory=(cwd+'/'))
 	plotter.create_generation_plot(all_statistics_of_all_runs, title="Statistics of every run of ")
